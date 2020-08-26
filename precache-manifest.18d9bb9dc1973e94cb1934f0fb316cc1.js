e8ade5e36c3867a564f7f8894f64c7ac61f8cbda
shashilsravan <shashilsravan.ss.ss@gmail.com>
2020-08-26 23:10:48 +0530

    Updates

---
+++ precache-manifest.18d9bb9dc1973e94cb1934f0fb316cc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904e704ad0338a22847023e972637126",
+    "url": "/recipeFinder/index.html"
+  },
+  {
+    "revision": "c61685f201692c152582",
+    "url": "/recipeFinder/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0f47584d0db83e921d2e",
+    "url": "/recipeFinder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c61685f201692c152582",
+    "url": "/recipeFinder/static/js/2.2c004bb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeFinder/static/js/2.2c004bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f47584d0db83e921d2e",
+    "url": "/recipeFinder/static/js/main.75e9edc2.chunk.js"
+  },
+  {
+    "revision": "7cc2c2e907754e508c39",
+    "url": "/recipeFinder/static/js/runtime-main.62aa2cbf.js"
+  }
+]);
